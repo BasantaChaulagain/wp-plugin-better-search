58a81f0b62ea57e64400021275e4d93e3bbbf7f7
Basanta Chaulagain <basanta@Basantas-MacBook-Pro.local>
2021-10-07 22:32:49 -0400

    Initial commit

---
+++ main.php
@@ -0,0 +1,25 @@
+<?php
+/* 
+In the db, form_value should be unserialized php array with 'cfdb7_status' as key and "read/unread" as value.
+*/
+
+//user input area
+$_GET['fid'] = 4;
+$_GET['ufid'] = 4;
+
+$_REQUEST['action'] = "unread";
+$_POST['contact_form'] = ['form_id'=>"4"];
+
+$_REQUEST['fid'] = 0;
+$_REQUEST['wpforms-csv'] = "";
+$_REQUEST['nonce'] = 'dnonce';
+
+//include fake_wp
+include "fake_wp.php";
+
+//include target function
+include "./better-search-replace/better-search-replace.php";
+
+//triger target function
+// creates an object of class Better_Search_Replace and runs it.
+run_better_search_replace();
